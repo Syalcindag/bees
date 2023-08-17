b4af5ba88cb732bc5caaf324faa0b0c5cec0ddff
Syalcindag <sefayalcindag1741@gmail.com>
2023-08-17 22:51:23 +0300

    add Info component and import it

---
+++ app/components/Info.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Info = () => {
+  return (
+    <div className='w-full'>
+        <div className='p-4 text-slate-800 bg-slate-300 text-[14px] md:text-[18px] lg:text-2xl text-left md:text-center'> 
+            <p>Hey honey lover 🐝, this is <span className='font-bold'> the best store </span> to buy honey</p>
+        </div>
+    </div>
+  )
+}
+
+export default Info
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Hero from "./components/Hero"
+import Info from "./components/Info"
 
 export const metadata = {
   title: 'Home Page',
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <>
       <Hero/>
+      <Info/>
     </>
   )
 }
