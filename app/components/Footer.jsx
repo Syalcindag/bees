0810c194fee942bb45f29b169e61eb0da4d7a108
Syalcindag <sefayalcindag1741@gmail.com>
2023-08-18 18:05:43 +0300

    add footer component and contact page

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='py-5 bg-black text-white'>
+      <p className='font-bold text-1xl text-center text-[var(--primary-color)]'>Copyright © 2023 Syalcindag</p>
+    </footer>
   )
 }
 
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
+export const metadata = {
+    title: 'Contact'
+}
 const page = () => 
 {
     return (
-        <div className="mt-[200px]">Contact Page</div>
+        <div className="text-center mt-[180px] h-[75vh]">
+            <h1 className="font-bold text-2xl uppercase pt-8">Contact Page here!</h1>
+        </div>
     )
 }
 
