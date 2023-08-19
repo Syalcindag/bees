085940d64e2c7ce06ae8724145e48be62c99d929
Syalcindag <sefayalcindag1741@gmail.com>
2023-08-19 15:38:12 +0300

    update css class

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
   return (
     <nav className="w-full bg-[var(--primary-color)] text-white fixed top-0 right-0 z-10">
         <div className="justify-between px-4 mx-auto md:flex md:items-center md:px-8 lg:max-w-6xl">
-            <div className="flex justify-between items-center py-3 md:py-6">
+            <div className="flex justify-between items-center py-3 md:py-3">
                 <Link href="/">
                     <h2 id="headerTitle" className="text-3xl text-black font-bold uppercase md:text-2xl">BalSefası</h2>
                 </Link>
